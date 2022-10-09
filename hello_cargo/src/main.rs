c0d2594c8498df1843deefd4ec7320a76d713f99
cx33 <lne128gene980@163.com>
2022-10-09 22:20:21 +0800

    feat: guess game

---
+++ hello_cargo/src/main.rs
@@ -0,0 +1,29 @@
+use rand::Rng;
+use std::{cmp::Ordering, io};
+
+fn main() {
+    let num = rand::thread_rng().gen_range(1..=10);
+
+    println!("请输入一个数字");
+
+    loop {
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("读取行失败");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&num) {
+            Ordering::Less => println!("太小了"),
+            Ordering::Greater => println!("太大了"),
+            Ordering::Equal => {
+                println!("猜对了");
+                println!("随机生成的数字是: {}", num);
+                break;
+            }
+        }
+        println!("你输入的数字是: {}", guess);
+    }
+}
