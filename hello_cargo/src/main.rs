5314f39845ad2fe0d285b3a1c6ea0eaabce18957
cx33 <lne128gene980@163.com>
2022-10-14 20:33:20 +0800

    feat: minigrep

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,33 +1,27 @@
-use rand::Rng;
-use std::{cmp::Ordering, io};
+use std::{env, fs};
 
 fn main() {
-    let num = rand::thread_rng().gen_range(1..=10);
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args);
+    println!(
+        "fileName is {}, query is {}.",
+        config.file_name, config.query
+    );
 
-    for i in (1..4).rev() {
-        println!("{}", i);
-    }
+    let content = fs::read_to_string(config.file_name).expect("文件读取错误");
+    println!("The content is:\n{}", content);
+}
 
-    println!("请输入一个数字");
+struct Config {
+    file_name: String,
+    query: String,
+}
 
-    loop {
-        let mut guess = String::new();
-        io::stdin().read_line(&mut guess).expect("读取行失败");
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let file_name = args[1].clone();
+        let query = args[2].clone();
 
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        match guess.cmp(&num) {
-            Ordering::Less => println!("太小了"),
-            Ordering::Greater => println!("太大了"),
-            Ordering::Equal => {
-                println!("猜对了");
-                println!("随机生成的数字是: {}", num);
-                break;
-            }
-        }
-        println!("你输入的数字是: {}", guess);
+        Config { file_name, query }
     }
 }
