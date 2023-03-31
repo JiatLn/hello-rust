eee903efe9fc723e988818c0d5b6b7069c084d34
cx33 <lne128gene980@163.com>
2023-03-31 21:21:22 +0800

    feat: 简易的web服务器

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,10 +1,14 @@
 use std::{
+    fs,
     io::prelude::*,
     net::{TcpListener, TcpStream},
 };
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    println!("Listening on port 7878...");
+    println!("http://127.0.0.1:7878");
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
@@ -18,5 +22,26 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     println!("Connection established!");
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]))
+    // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    let get = b"GET / HTTP/1.1\r";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let filename = format!("pages/{}", filename);
+    let contents = fs::read_to_string(&filename).unwrap();
+
+    let response = format!(
+        "{}\r\nContent-Type: text/html\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
